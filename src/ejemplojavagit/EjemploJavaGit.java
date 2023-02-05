abdf81c928ad87ebd7e40843f566d799c68cf061
Sandra <usuario@hotmail.com>
2023-02-05 16:53:16 +0100

    primero commit

--- src/ejemplojavagit/EjemploJavaGit.java
+++ src/ejemplojavagit/EjemploJavaGit.java
@@ -15,6 +15,7 @@ public class EjemploJavaGit {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Prueba github");
     }
     
 }
