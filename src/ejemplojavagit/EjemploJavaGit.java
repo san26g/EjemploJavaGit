8f8af6d99d31bbe199989f7468313958f65c5504
rootsandra <rootsandra@A51E03-Y17.infor.com>
2023-02-06 17:41:49 +0100

    tercer commit

--- src/ejemplojavagit/EjemploJavaGit.java
+++ src/ejemplojavagit/EjemploJavaGit.java
@@ -16,6 +16,7 @@ public class EjemploJavaGit {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Prueba github");
+        System.out.println("Segunda prueba");
     }
     
 }
